61e0e42bce1df7a7fa7d161f11bc9ca04cb79b6a
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-03-27 17:28:04 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import React from "react";
+
 function App() {
+  const products = [
+    { name: "Mela", price: 0.5 },
+    { name: "Pane", price: 1.2 },
+    { name: "Latte", price: 1.0 },
+    { name: "Pasta", price: 0.7 },
+  ];
+
 
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>Prodotti</h1>
+      {products.map((product) => (
+        <div key={product.name}>
+          <p>Nome: {product.name}</p>
+          <p>Prezzo: {product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
