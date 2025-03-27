d5734237d0062c24fca14df3112193a80fe38516
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-03-27 18:01:36 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   const products = [
@@ -8,18 +8,45 @@ function App() {
     { name: "Pasta", price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  function addToCart(product) {
+    setAddedProducts((prevAddedProducts) => {
+      const productExists = prevAddedProducts.some((p) => p.name === product.name);
+      if (productExists) {
+        return prevAddedProducts;
+      }
+      return [...prevAddedProducts, { ...product, quantity: 1 }];
+    });
+  };
 
   return (
     <div>
       <h1>Prodotti</h1>
-      {products.map((product) => (
-        <div key={product.name}>
+      {products.map((product, index) => (
+        <div key={index}>
           <p>Nome: {product.name}</p>
           <p>Prezzo: {product.price}</p>
+          <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
         </div>
       ))}
+
+      {addedProducts.length > 0 && (
+        <div>
+          <h2>Prodotti nel carrello</h2>
+          <ul>
+            {addedProducts.map((product, index) => (
+              <li key={index}>
+                <p>Nome: {product.name}</p>
+                <p>Prezzo: {product.price}</p>
+                <p>Quantità: {product.quantity}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
